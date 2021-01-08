dd115defd95a32619983169777581178f5396565
Ed Skoviak <eskoviak@eskoviak.com>
2021-01-08 12:33:57 -0600

    added repo and sql server

---
+++ scripts/sql/helper.sql
@@ -0,0 +1,26 @@
+SELECT *
+FROM Occupation;
+
+SELECT COUNT(*)
+FROM Participant;
+
+SELECT *
+FROM [User];
+
+SELECT *
+FROM UserTestingType
+
+SELECT DISTINCT TestingTypeID
+FROM Participant;
+
+SELECT *
+FROM TestingStatus;
+
+SELECT COUNT(*)
+FROM Style;
+
+SELECT TOP 10 *
+FROM Style;
+
+SELECT *
+FROM INFORMATION_SCHEMA.TABLES;
